67abfc75ac7a267d7b35134b8f4847ccb7c23ab9
Scott <scottlewwork@gmail.com>
2024-04-18 10:49:47 +0100

    Updated Git ignore

--- Tapper/main.cpp
+++ Tapper/main.cpp
@@ -28,10 +28,14 @@ int main(int argc, char* argv[])
 	sideWall->LoadImage("Side Wall.bmp");
 	sdlEngine.AddLayerElement(sideWall, 5);
 
+	Uint64 start{ 0 };
+	Uint64 end{ 0 };
+	float elapsed{ 0 };
 
 	bool loop = true;
 	while (loop)
 	{
+		start = SDL_GetPerformanceCounter();
 		sdlEngine.Reset();
 		while (SDL_PollEvent(&e))
 		{
@@ -42,6 +46,9 @@ int main(int argc, char* argv[])
 		}
 		sdlEngine.Update(e);
 		sdlEngine.Present();
+		end = SDL_GetPerformanceCounter();
+		elapsed = (end - start) / (float)SDL_GetPerformanceFrequency();
+		std::cout << "Current FPS: " << 1.0f / elapsed << std::endl;
 	}
 
 
